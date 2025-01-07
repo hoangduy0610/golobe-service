460d15fe88d7bdc9f40dcfa87b64eb5376512000
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2025-01-07 18:42:15 +0700

    cool ai

--- src/services/ChatService.ts
+++ src/services/ChatService.ts
@@ -2,11 +2,13 @@ import { ApplicationException } from '@/controllers/ExceptionController';
 import { Location } from '@/entities/Location.entity';
 import { Service } from '@/entities/Service.entity';
 import { ServiceType } from '@/entities/ServiceType.entity';
+import { FaissStore } from "@langchain/community/vectorstores/faiss";
 import { ChatPromptTemplate } from '@langchain/core/prompts';
-import { AzureChatOpenAI } from '@langchain/openai';
+import { AzureChatOpenAI, AzureOpenAIEmbeddings } from '@langchain/openai';
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { HttpResponseOutputParser } from 'langchain/output_parsers';
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -17,51 +19,50 @@ export class ChatService {
         @InjectRepository(Service) private readonly serviceRepository: Repository<Service>
     ) { }
 
-    async processUserMessage(userMessage: string): Promise<string> {
+    async processUserMessage(userMessage: string): Promise<any> {
         try {
+            const model = new AzureChatOpenAI({
+                temperature: 0.01,
+                model: "gpt-4o",
+                maxRetries: 2,
+            });
+            const embeddings = new AzureOpenAIEmbeddings({ maxRetries: 1 });
+
             // Retrive data from DB
             const locationData = await this.locationRepository.find();
             const serviceData = await this.serviceRepository.find({
                 relations: ['serviceType']
             });
-
+            const locationDataString = locationData.map(location => `- Địa điểm: ${location.name}, địa chỉ: ${location.address}`).join('\n');
+            const serviceString = serviceData.map(service => `- Dịch vụ: ${service.name}, loại dịch vụ: ${service.serviceType.name}, địa chỉ: ${service.address}, số điện thoại: ${service.phone}, email: ${service.email}, website: ${service.website}, mức giá: ${service.priceRange}`).join('\n');
+            
             // Split system prompt and user prompt
-            let systemPrompt = `You're an AI assistant that helps users planning their trip. You can provide information about locations, services, and service types. You can also help users to find the best service for their needs. Do not makeup any data.`;
-            let userPrompt = `These are the data that you can use.
-<LOCATION>
-<SERVICE>
-I need you to help me with the following user message: {input}. Please remember to answer in Vietnamese`;
+            let systemPrompt = `Bạn là trợ lý AI giúp người dùng lập kế hoạch cho chuyến đi của họ. Bạn có thể cung cấp thông tin về địa điểm, dịch vụ và loại dịch vụ. Bạn cũng có thể giúp người dùng tìm dịch vụ tốt nhất cho nhu cầu của họ. Không được tạo ra bất kỳ dữ liệu nào.`;
+            let userPrompt = `Hãy giúp tôi trả lời câu hỏi sau: {input}\nsử dụng các dữ liệu sau:\n\n{context}`;
 
-            // Fill in the blanks
-            const locationDataString = locationData.map(location => `- Location Name: ${location.name}, address: ${location.address}`).join(', ');
-            const serviceString = serviceData.map(service => `- Service Name: ${service.name}, type of service: ${service.serviceType.name}, address: ${service.address}, phone: ${service.phone}, email: ${service.email}, website: ${service.website}, price range: ${service.priceRange}`).join(', ');
-            userPrompt.replace('<LOCATION>', locationDataString);
-            userPrompt.replace('<SERVICE>', serviceString);
+            const store = await FaissStore.fromTexts(
+                [locationDataString, serviceString],
+                ["location", "service"],
+                embeddings
+            );
 
-            const prompt = ChatPromptTemplate.fromMessages([
-                ['system', systemPrompt],
-                ['human', userPrompt],
-            ]);
-
-            const model = new AzureChatOpenAI({
-                temperature: 0.01,
-                model: "gpt-4o",
-                // azureOpenAIApiKey: "<your_key>", // In Node.js defaults to process.env.AZURE_OPENAI_API_KEY
-                // azureOpenAIApiInstanceName: "<your_instance_name>", // In Node.js defaults to process.env.AZURE_OPENAI_API_INSTANCE_NAME
-                // azureOpenAIApiDeploymentName: "<your_deployment_name>", // In Node.js defaults to process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME
-                // azureOpenAIApiVersion: "<api_version>", // In Node.js defaults to process.env.AZURE_OPENAI_API_VERSION
-                maxRetries: 2,
+            const tempPrompt = ChatPromptTemplate.fromMessages([
+                ["system", systemPrompt],
+                ["human", userPrompt],
+            ])
+            
+            const combineDocsChain = await createStuffDocumentsChain({
+                llm: model,
+                prompt: tempPrompt,
             });
 
-            const outputParser = new HttpResponseOutputParser();
-            const chain = prompt.pipe(model).pipe(outputParser);
-            const response = await chain.invoke({
-                input: userMessage,
+            const chain = await createRetrievalChain({
+                retriever: store.asRetriever(),
+                combineDocsChain,
             });
 
-            return Object.values(response)
-                .map((code) => String.fromCharCode(code))
-                .join('')
+            const response = await chain.invoke({ input: userMessage });
+            return response.answer;
         } catch (e: any) {
             console.log(e);
             throw new ApplicationException(HttpStatus.BAD_REQUEST, 'Error processing user message');
